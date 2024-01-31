a671fbe81781748b06e880e6e8c0d6db0a7ff510
CUBE <applepie090902@gmail.com>
2024-01-30 23:43:31 -0500

    selecting encode/decode

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,37 @@ fn main() {
     println!("Input the path of the file:");
     let mut path = String::new();
     let _ = io::stdin().read_line(&mut path);
-    match reader(&path.trim()) {
-        Ok(content) => {
-            let output = encode::encode_base64(&content);
-            writer(&path.trim(), &output);
-            println!("{}", output);
+    println!("Please select one option");
+    println!("1: Encode");
+    println!("2: Decode");
+    let mut option = String::new();
+    let _ = io::stdin().read_line(&mut option);
+
+    match option.trim().parse::<u8>() {
+        Ok(u8_selected) =>
+            match u8_selected {
+                1 => {
+                    match reader(&path.trim()) {
+                        Ok(content) => {
+                            let output = encode::encode_base64(&content);
+                            writer(&path.trim(), &output);
+                            println!("{}", output);
+                        },
+                        Err(_) => eprintln!(),
+                    }
+                },
+                2 => {
+                    match reader(&path.trim()) {
+                        Ok(content) => {
+                            let output = decode::decode_base64(&content);
+                            writer(&path.trim(), &output);
+                            println!("{}", output);
+                        },
+                        Err(_) => eprintln!(),
+                    }
+                },
+                _ => eprintln!("Option not accepted.")  
         },
-        Err(_) => eprintln!(),
+        Err(e) => eprintln!("{}",e)
     }
 }
